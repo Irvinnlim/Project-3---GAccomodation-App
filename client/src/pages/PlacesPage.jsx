0264fb22d3d0a61356a5e9cd9f41db1abcfc1736
Louiskok888 <115003571+Louiskok888@users.noreply.github.com>
2023-03-21 22:14:21 +0800

    Places form completed

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -17,12 +17,17 @@ export default function PlacesPage() {
             {action === 'new' && (
                 <div>
                     <form>
-                        <h2 className="text -xl mt-4">Title</h2>
-                        <p className="text-gray-500">title for your place. should be short and catchy as in advertisement</p>
+                        <h2 className="text -2xl mt-4">Title</h2>
+                        <p className="text-gray-500 text-sm">title for your place. should be short and catchy as in advertisement</p>
                         <input type="text" placeholder="title, for example: My lovely apt"/> 
-                        <h2 className="text -xl mt-4">Address</h2>
+                        <h2 className="text -2xl mt-4">Address</h2>
+                        <p className="text-gray-500 text-sm">Address to this place</p>
                         <input type="text" placeholder="address"/>
-                        <h2 className="text -xl mt-4">Photos</h2>
+                        <h2 className="text -2xl mt-4">Photos</h2>
+                        <p className="text-gray-500 text-sm">more = better</p>
+                        <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+                            <button className="border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">+</button>
+                        </div>
 
                     </form>
                 </div>
@@ -30,7 +35,3 @@ export default function PlacesPage() {
         </div>
     );
 }
-//jhkjfjkjhkjhkgjhghfyddhdhhgghjjkjkhhkljlkjl
-//bdfhdgsfjkafnasna
-//mnbsbmsabbs,mndnsknks/msbsmb/
-//smbmnsbsbbs
