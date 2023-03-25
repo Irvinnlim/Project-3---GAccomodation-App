c3538b55ec388b19651d79d948449aeaed3c31e4
bengyh <115003549+bengyh@users.noreply.github.com>
2023-03-25 17:45:31 +0900

    view in single page

--- client/src/AccountNav.js
+++ client/src/AccountNav.js
@@ -2,7 +2,7 @@ import { Link, useLocation } from "react-router-dom";
 
 export default function AccountNav() {
   const { pathname } = useLocation();
-  const subpage = pathname.split("/")?.[2];
+  let subpage = pathname.split("/")?.[2];
   if (subpage === undefined) {
     subpage = "profile";
   }
