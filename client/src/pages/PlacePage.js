492e549b7ecc0cbf499862f457ef22d9a946e637
irvinnlim <irvinnlim@gmail.com>
2023-03-27 12:38:02 +0800

    added perks in PlacePage

--- client/src/pages/PlacePage.js
+++ client/src/pages/PlacePage.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import BookingWidget from "../BookingWidget";
 import PlaceGallery from "../PlaceGallery";
 import AddressLink from "../AddressLink";
+import Perks from "../Perks";
 
 export default function PlacePage() {
   const { id } = useParams();
